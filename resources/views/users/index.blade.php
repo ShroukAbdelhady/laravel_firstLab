c19ae3e6df4d9e57216f357ac56d1cc04ab2b00a
ShroukAbdelhady <shroukemad66@gmail.com>
2022-10-23 14:52:18 +0200

    update

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -8,6 +8,12 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 </head>
 <body>
+   <div style="float:right">
+    <form action="{{ route('user.create')}}" class="d-inline">
+         <button class="btn btn-info"> Add User </button>
+        </form>
+   </div>
+
 <table class="table">
   <thead class="table-dark">
     <tr>
@@ -26,9 +32,6 @@
       <td>{{ $user->name }}</td>
       <td>{{ $user->email }}</td>
       <td >
-        <form action="{{ route('user.create')}}" class="d-inline">
-         <button class="btn btn-info"> Add </button>
-        </form>
          <form action="{{ route('user.show',$user->id) }}" class="d-inline" >
          <button class="btn btn-success">show</button>
         </form>
